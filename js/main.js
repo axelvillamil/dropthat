514ff0a2a17dbace8b81dbfede8cbbbd49f048aa
axelvillamil <axelvillamil@gmail.com>
2015-11-30 03:31:28 -0500

    trying one more time

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-// $(window).on('load',carouselWidth);
-// $(window).on('resize',carouselWidth);
-//
-// function carouselWidth() {
-//   var x = $( window ).height();
-//   var new_height = x/2;
-//
-//   $("#icon_set").height(new_height);
-//
-//   console.log(new_height)
-// };
+$(window).load(carouselWidth);
+$(window).on('resize',carouselWidth);
+
+$(document).ready(function() {
+
+  $(".down-btn").click(function () {
+    console.log("Hello!");
+  });
+
+});
+
+
+function carouselWidth() {
+  var x = $( window ).height();
+  var new_height = x/2;
+  // var y = $(".grid-img").width();
+
+  $("#icon_set").height(new_height);
+  // $(".effect-bubba").width(y);
+  console.log(new_height)
+};
