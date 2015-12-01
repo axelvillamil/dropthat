e2467b76acc6699e5199bc770617b2d8dfddf62d
axelvillamil <axelvillamil@gmail.com>
2015-11-30 23:18:52 -0500

    responsive fixes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-// $(window).on('load',carouselWidth);
-// $(window).on('resize',carouselWidth);
-//
-// function carouselWidth() {
-//   var x = $( window ).height();
-//   var new_height = x/2;
-//
-//   $("#icon_set").height(new_height);
-//
-//   console.log(new_height)
-// };
+$(window).on('load',carouselWidth);
+$(window).on('resize',carouselWidth);
+
+function carouselWidth() {
+  var x = $( window ).height();
+  var new_height = x/2;
+
+  $("#icon_set").height(new_height);
+
+  console.log(new_height)
+};
